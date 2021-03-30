55dbf2443a29f108e87c5e1c9f0607a0fef4c702
Shubham Gupta <xblacklotus.22@gmail.com>
2021-03-30 22:24:14 +0530

    image-save

--- objid/identify/views.py
+++ objid/identify/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .forms import ImageUploadForm
+
+def handle_uploaded_file(f):
+   with open('img.jpg', 'wb+') as destination:
+      for chunk in f.chunks():
+         destination.write(chunk)
 
 def home(request):
    return render(request,'home.html')
 
 def imageprocess(request):
+   form = ImageUploadForm(request.POST, request.FILES)
+   if form.is_valid():
+      handle_uploaded_file(request.FILES['image'])
+
    return render(request,'results.html')
